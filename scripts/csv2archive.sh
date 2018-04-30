33c4f59720c38b1b5157b7bd78c93f7ee9f58953
shaunbell <shaun.bell@noaa.gov>
2018-04-30 09:37:39 -0700

    for loop fix

--- scripts/csv2archive.sh
+++ scripts/csv2archive.sh
@@ -37,6 +37,8 @@ argosID="028882"
 for id in $argosID
 do
    	for fid in ${path}${files}
-    echo "processing file: ${id} - ${fid}"
-	python SOAP2ArchiveCSV.py ${fid} --buoyyearid ${path}data/${files}.${id}.y${year} 
+   	do
+	    echo "processing file: ${id} - ${fid}"
+		python SOAP2ArchiveCSV.py ${fid} --buoyyearid ${path}data/${files}.${id}.y${year} 
+	done
 done
