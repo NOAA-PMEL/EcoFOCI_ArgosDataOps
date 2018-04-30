1493b610ce0e425b2c10f7f6b4c2bdd5b426549b
shaunbell <shaun.bell@noaa.gov>
2018-04-30 09:35:56 -0700

    update syntax

--- scripts/csv2archive.sh
+++ scripts/csv2archive.sh
@@ -36,7 +36,7 @@ argosID="028882"
 
 for id in $argosID
 do
-   	for fid in ${path}$files
-    echo "processing file: $id - $fid"
-	python SOAP2ArchiveCSV.py ${fid} --buoyyearid ${path}data/$files.${id}.y${year} 
+   	for fid in ${path}${files}
+    echo "processing file: ${id} - ${fid}"
+	python SOAP2ArchiveCSV.py ${fid} --buoyyearid ${path}data/${files}.${id}.y${year} 
 done
