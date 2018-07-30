e49dc49ac1e16f3349d2f5a2883c68a649590065
shaunwbell <shaun.bell@noaa.gov>
2018-07-30 13:18:07 -0700

    ws

--- scripts/SOAP2CSV_byid.sh
+++ scripts/SOAP2CSV_byid.sh
@@ -3,6 +3,8 @@
 # Purpose:
 #       Script to run SOAP2ArchiveCSV.py for each file in a directory
 #       and output as concatenated file - only makes year files but not translated 
+#
+# 
 
 year=2018
 path="/home/pavlof/bell/Programs/Python/EcoFOCI_ArgosDataOps/data/*.csv"
