f2dd2d95e22d98da58e47c4dd77a170fa99c4309
shaunwbell <shaun.bell@noaa.gov>
2018-07-30 12:32:43 -0700

    path to programs

--- scripts/SOAP2CSV_byid.sh
+++ scripts/SOAP2CSV_byid.sh
@@ -5,11 +5,12 @@
 #       and output as concatenated file - only makes year files but not translated 
 
 year=2018
-path="/home/pavlof/bell/Programs/Python/EcoFOCI_ArgosDataOps/data/raw_data/*.csv"
+path="/home/pavlof/bell/Programs/Python/EcoFOCI_ArgosDataOps/data/*.csv"
+progdir="/home/pavlof/bell/Programs/Python/EcoFOCI_ArgosDataOps/"
 
 for files in $path
 do
     echo "processing file: $files"
-	python SOAP2ArchiveCSV.py ${files} -drifteryearfiles
+	python ${progdir}SOAP2ArchiveCSV.py ${files} -drifteryearfiles
 done
 
