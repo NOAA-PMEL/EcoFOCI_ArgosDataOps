6ce4f5e805b11bf2cfdcde1419ff2c1ea004ab1d
shaunbell <shaun.bell@noaa.gov>
2018-07-31 08:54:38 -0700

    loop to skip missing files

--- scripts/SOAPCSV2nc_current.sh
+++ scripts/SOAPCSV2nc_current.sh
@@ -7,22 +7,23 @@
 # Be sure to use an appropriate environment (ARGODrifters-py36 or ARGODrifters)
 
 year=2018
-path="/home/pavlof/bell/Programs/Python/EcoFOCI_ArgosDataOps/data/year_archive/*.y${year}"
-buoyfile="/home/pavlof/bell/Programs/Python/EcoFOCI_ArgosDataOps/scripts/v2_buoy_ids.csv"
+path="/Users/bell/Programs/Python/EcoFOCI_ArgosDataOps/data/year_archive/*.y${year}"
+buoyfile="/Users/bell/Programs/Python/EcoFOCI_ArgosDataOps/scripts/v2_buoy_ids.csv"
 
 for files in $path
 do
     names=(${files//\// })
     outfile=${names[${#names[@]} - 1]}
 
-	if [[ "$outfile" =~ $(echo ^\($(paste -sd'|' ${buoyfile})\)$) ]]; then
-		#version2 buoys
-	    echo "processing file: $files"
-		python ARGOS_service_data_converter.py ${files} v2 -nc data/${files}.nc
-	else
-		#metocean buoys - id's listed in a seperate file
-	    echo "processing file: $files"
-		python ARGOS_service_data_converter.py ${files} v1 -nc data/${files}.nc
+    if [ -s ${files} ]; then
+		if [[ "$outfile" =~ $(echo ^\($(paste -sd'|' ${buoyfile})\)$) ]]; then
+			#version2 buoys
+		    echo "processing file: $files"
+			python ARGOS_service_data_converter.py ${files} v2 -nc data/${outfile}.nc
+		else
+			#metocean buoys - id's listed in a seperate file
+		    echo "processing file: $files"
+			python ARGOS_service_data_converter.py ${files} v1 -nc data/${outfile}.nc
+		fi
 	fi
-
 done
